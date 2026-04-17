17153b3852a3b42ec4df407366e28deb2747a28f
Dione <dionepinheiro6030@gmail.com>
2026-04-17 20:14:32 -0300

    🐛 Atualiza validações e estrutura das rotas de jogador e seleção

--- Aula 5/src/routes/jogador.ts
+++ Aula 5/src/routes/jogador.ts
@@ -1,23 +1,61 @@
 import { prisma } from "../../lib/prisma"
 import { Router } from "express"
 import { z } from "zod"
+import { Posicao } from "../../generated/prisma/enums"
 
 const router = Router()
 
 const jogadorSchema = z.object({
-    nome: z.string(),
-    posicao: z.string(),
-    dataNasc: z.string().datetime({ message: "Data de nascimento deve estar no formato UTC!"}),
-    numCamisa: z.number().max(26, { message: "Número da camisa deve ser entre 1 e 26!" }),
-    selecaoId: z.number()
+    nome: z.string().max(60, { message: "O nome do jogador deve conter no máximo 60 caracteres!" }),
+    posicao: z.enum(Posicao),
+    selecaoId: z.number().int().positive('ID da seleção deve ser um número positivo'),
+    dataNasc: z.string()
+        .regex(/^\d{4}-\d{2}-\d{2}$/, "Formato deve ser YYYY-MM-DD")
+        .transform((value) => new Date(value))
+        .refine((date) => !isNaN(date.getTime()), {
+            message: "Data inválida"
+        }),
+    numCamisa: z.number().int().min(1).max(26, 'Número da camisa entre 1 e 26'),
 })
 
 router.get("/", async (req, res) => {
     try {
-        const jogadores = await prisma.jogador.findMany()
-        res.status(200).json(jogadores)
+        const jogadores = await prisma.jogador.findMany({
+            include: { selecao: true }
+        })
+        
+        const jogadores2 = jogadores.map(jogador => ({
+            id: jogador.id,
+            nome: jogador.nome,
+            posicao: jogador.posicao,
+            numCamisa: jogador.numCamisa,
+            selecao: jogador.selecao.pais
+        }))
+        
+        res.status(200).json(jogadores2)
     } catch (error) {
         res.status(500).json({ erro: "Erro de servidor..."})
+    }
+})
+
+router.post("/", async (req, res) => {
+    const valida = jogadorSchema.safeParse(req.body)
+
+    if (!valida.success) {
+        res.status(400).json({ erro: valida.error })
+        return
+    }
+
+    // Desestrutura os dados validados
+    const { nome, posicao, selecaoId, dataNasc, numCamisa } = valida.data
+
+    try {
+        const jogador = await prisma.jogador.create({
+            data: { nome, posicao, selecaoId, dataNasc, numCamisa }
+        })
+        res.status(201).json(jogador)
+    } catch (error) {
+        res.status(500).json({ error })
     }
 })
 
--- Aula 5/src/routes/selecao.ts
+++ Aula 5/src/routes/selecao.ts
@@ -5,21 +5,60 @@ import { z } from "zod"
 const router = Router()
 
 const selecaoSchema = z.object({
-    pais: z.string(),
-    continente: z.string(),
-    treinador: z.string(),
-    numCopas: z.number()
+    pais: z.string()
+        .min(3, { message: "O nome do país deve conter no mínimo 3 caracteres!" })
+        .max(50, { message: "O nome do país deve conter no máximo 50 caracteres!" }),
+    continente: z.string().max(20, { message: "O nome do continente deve conter no máximo 20 caracteres!" }),
+    treinador: z.string().max(40, { message: "O nome do treinador deve conter no máximo 40 caracteres!" }),
+    numCopas: z.number().int().min(0).optional()
 })
 
 router.get("/", async (req, res) => {
     try {
-        const selecoes = await prisma.selecao.findMany()
-        res.status(200).json(selecoes)
+        const selecoes = await prisma.selecao.findMany({
+            include: { jogadores: true }
+        })
+
+        const selecoes2 = selecoes.map(selecao => ({
+            id: selecao.id,
+            pais: selecao.pais,
+            continente: selecao.continente,
+            treinador: selecao.treinador,
+            numCopas: selecao.numCopas,
+            jogadores: selecao.jogadores.map(jogador => ({
+                id: jogador.id,
+                nome: jogador.nome,
+                posicao: jogador.posicao,
+                numCamisa: jogador.numCamisa,
+                dataNasc: jogador.dataNasc
+            }))
+        }))
+        res.status(200).json(selecoes2)
+
     } catch (error) {
         res.status(500).json({ erro: "Erro no servidor" })
     }
 })
 
+router.post("/", async (req, res) => {
+    const valida = selecaoSchema.safeParse(req.body)
+    if (!valida.success) {
+        res.status(400).json({ erro: valida.error })
+        return
+    }
+
+    // Desestrutura os dados validados
+    const { pais, continente, numCopas, treinador } = valida.data
+
+    try {
+        const selecao = await prisma.selecao.create({
+            data: { pais, continente, numCopas, treinador }
+        })
+        res.status(201).json(selecao)
+    } catch (error) {
+        res.status(500).json({ error })
+    }
+})
 
 /*
 router.put("/:id", async (req, res) => {
